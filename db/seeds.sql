113658bb02842548814609bee53ea254a48e9578
Woogster <john.m.wooley@gmail.com>
2022-03-11 22:41:22 -0600

    Starting seeding

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO department
+    (id, name)
+VALUES
+    (1, Business)
+    (2, Legal)
+    (3, Cleaning)
+
+
+INSER INTO role
+    (id, title, salary, department_id)
+VALUES
+    (1, Lawyer, 200000, 2)
+
+
+
+INSERT INTO employee 
+    (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (1, John, Johnson, 2, 3)
+    ()
