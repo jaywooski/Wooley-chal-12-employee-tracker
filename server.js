cbbefec576e4773b73addfa3155df1878d661c81
Woogster <john.m.wooley@gmail.com>
2022-03-12 16:14:07 -0600

    Adding and adjusting package json file to initialize app from server.js

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
-const express = require('express');
 const mysql = require('mysql2');
-const PORT = process.env.PORT || 3002;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json());
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 
-
-app.listen(PORT, () => {
-    console.log(`Server now active on port ${PORT}!!`);
-});
